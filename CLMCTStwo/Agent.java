fbd7e78e1fa4b2e0678ee3cb0f8de7b50ab6d2d2
TY <1015024572@qq.com>
2019-08-03 22:18:28 +0800

    CLMCTS2.0 commit

---
+++ CLMCTStwo/Agent.java
@@ -0,0 +1,61 @@
+package CLMCTStwo;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import core.game.StateObservation;
+import core.player.AbstractPlayer;
+import ontology.Types;
+import tools.ElapsedCpuTimer;
+
+
+public class Agent extends AbstractPlayer {
+
+    public int num_actions;
+    public Types.ACTIONS[] actions;
+
+    protected SingleMCTSPlayer mctsPlayer;
+
+    /**
+     * Public constructor with state observation and time due.
+     * @param so state observation of the current game.
+     * @param elapsedTimer Timer for the controller creation.
+     */
+    public Agent(StateObservation so, ElapsedCpuTimer elapsedTimer)
+    {
+        //Get the actions in a static array.
+        ArrayList<Types.ACTIONS> act = so.getAvailableActions(true);
+        actions = new Types.ACTIONS[act.size()];
+        for(int i = 0; i < actions.length; ++i)
+        {
+            actions[i] = act.get(i);
+        }
+        num_actions = actions.length;
+
+        //Create the player.
+
+        mctsPlayer = new SingleMCTSPlayer(new Random(), num_actions, actions);
+    }
+
+
+
+    /**
+     * Picks an action. This function is called every game step to request an
+     * action from the player.
+     * @param stateObs Observation of the current state.
+     * @param elapsedTimer Timer when the action returned is due.
+     * @return An action for the current state
+     */
+    public Types.ACTIONS act(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
+
+        //Set the state observation object as the new root of the tree.
+        mctsPlayer.init(stateObs);
+
+        //Determine the action using MCTS...
+        int action = mctsPlayer.run(elapsedTimer);
+//        System.out.println(action);
+        //... and return it.
+        return actions[action];
+    }
+
+}
